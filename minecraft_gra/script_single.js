0eab280d64afb9dcf36e64c0fc29eba314529e00
polskipietra <109820324+polskipietra@users.noreply.github.com>
2023-04-18 09:46:52 +0200

    Add files via upload

---
+++ minecraft_gra/script_single.js
@@ -0,0 +1,43 @@
+const btn1 = document.querySelector('.create');
+const btn2 = document.querySelector('.delete');
+const worlds = document.querySelector('.worlds');
+
+function create(name) {
+	const worldElement = document.createElement('section');
+	const worldName = document.createElement('h2');
+	const worldImage = document.createElement('div');
+	const checkBox = document.createElement('input');
+	worlds.append(worldElement);
+	worldElement.classList.add('looks');
+	worldElement.append(worldImage);
+	worldImage.classList.add('image');
+	worldElement.append(worldName);
+	worldName.textContent = name;
+	checkBox.type = 'checkbox';
+	worldElement.append(checkBox);
+}
+function check() {
+	const name = prompt('podaj nazwę świata:');
+	if (name == '') {
+		alert('podaj nazwe świata');
+		const name = prompt('podaj nazwę świata:');
+	} else {
+		create(name);
+	}
+}
+
+function deleteWorld() {
+	const deleteCheckbox = document.querySelector("input[type='checkbox']:checked");
+	if (deleteCheckbox) {
+		let deleteSection = deleteCheckbox.parentNode;
+		// console.dir(deleteSection.parentNode);
+		deleteSection.parentNode.removeChild(deleteSection);
+	}
+	// worlds.remove(deleteSection);
+}
+btn1.addEventListener('click', check);
+btn2.addEventListener('click', deleteWorld);
+
+// setTimeout(() => {
+// 	create('testowy');
+// }, 1000);
